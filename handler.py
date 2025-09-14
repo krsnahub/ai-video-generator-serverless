44828ac73cdb949b37c0aa8757bffaef96f7c15b
krsnahub <krsnasolo@gmail.com>
2025-09-14 16:18:01 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,8 +1,11 @@
-# Minimal Version (less readable)
 import runpod
 from workflows import process_data
 
 def handler(event):
+    """
+    This is the handler function that will be called by RunPod.
+    """
     return process_data(event["input"])
 
+# Start the serverless worker
 runpod.serverless.start({"handler": handler})
